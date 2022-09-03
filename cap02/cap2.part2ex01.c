a0591680f32fdd2812778f5b54d19e75142a2047
Francisco Zampirolli <fzampirolli@gmail.com>
2022-09-03 13:04:14 -0300

    cap2.part2.ex01

---
+++ cap02/cap2.part2ex01.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+// MÉTODOS
+int ParImpar(int n) {
+  return n % 2;
+}
+
+float leia() {
+  int valor;
+  printf("Entre com um valor inteiro: ");
+  scanf("%d", &valor);
+  return valor;
+}
+
+int main(void) {
+  // ENTRADAS
+  int n;
+  n = leia();
+
+  // PROCESSAMENTO
+  int r = ParImpar(n);
+
+  //SAÍDA
+  printf("0=Par e 1=Impar : %d\n", r);
+  return 0;
+}
