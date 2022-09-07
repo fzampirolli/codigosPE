4b9f753de5eaa6d33c2d2cb7a9eca4987eb37df0
Francisco Zampirolli <fzampirolli@gmail.com>
2022-09-07 18:04:13 -0300

    str

--- cap3/cap3.part1ex03.c
+++ cap3/cap3.part1ex03.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
 int main() {
   float area;
-  int escolha, num;
+  int num;
+  char escolha;
   printf("1. Circulo\n");
   printf("2. Quadrado\n");
   printf("Escolha:\n");
 
-  scanf("%d", &escolha);
+  scanf("%c", &escolha);
 
   switch (escolha) {
-  case 1: {
+  case '1':
     printf("Raio:\n");
     scanf("%d", &num);
     area = 3.14 * num * num;
     printf("Area do circulo: ");
     printf("%.2f\n", area);
     break;
-  }
-  case 2: {
+
+  case '2':
     printf("Lado:\n");
     scanf("%d", &num);
     area = num * num;
     printf("Area do quadrado: ");
     printf("%.2f\n", area);
     break;
-  }
+
   default:
     printf("Escolha Incorreta!\n");
   }
